50d3b6511636b857fbed91529e37e345d2b13c30
Ngoc Nguyen <ngoc.nguyen@solislab.com>
2021-03-19 14:22:34 +0700

    A24Press-5 A24-55 Add public share file text field

--- schemas/fileDownloads.js
+++ schemas/fileDownloads.js
@@ -8,6 +8,12 @@ export default {
       type: 'string'
     },
     {
+      title: 'Dropbox/WeTransfer Link (Optional)',
+      description: 'This link will override the required file upload field below',
+      name: 'fileUrl',
+      type: 'string'
+    },
+    {
       name: 'file',
       title: 'File',
       type: 'file',
