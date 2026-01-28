043ea26d8ca47e4154878fbe92ead016c53e3e17
Gary <gary@heavy.dev>
2026-01-28 16:07:07 +0700

    :feat: Add sanity-plugin-media

--- sanity.config.js
+++ sanity.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
+import {media} from 'sanity-plugin-media'
 import {googleMapsInput} from '@sanity/google-maps-input'
 
 import {schemaTypes} from './schemas'
@@ -14,7 +15,8 @@ export default defineConfig({
 
   plugins: [
     structureTool(),
-    visionTool(),
+    visionTool(), 
+    media(),
     googleMapsInput({
       apiKey: process.env.SANITY_STUDIO_GOOGLE_MAPS_API_KEY,
     }),
