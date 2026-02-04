7eee7becd0f6c6019fc3cbc21f13914ece436617
Gary <gary@heavy.dev>
2026-02-04 16:40:43 +0700

    :chore: Update enviroment

--- sanity.config.js
+++ sanity.config.js
@@ -8,20 +8,17 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'A24 Press',
-
-  projectId: 'xq1bjtf4',
-  dataset: 'production',
-
+  title: process.env.SANITY_STUDIO_API_SITE_TITLE || 'A24 Press',
+  projectId: process.env.SANITY_STUDIO_API_PROJECT_ID || 'xq1bjtf4',
+  dataset: process.env.SANITY_STUDIO_API_DATASET || 'production',
   plugins: [
     structureTool(),
     visionTool(), 
     media(),
     googleMapsInput({
-      apiKey: process.env.SANITY_STUDIO_GOOGLE_MAPS_API_KEY,
+      apiKey: process.env.SANITY_STUDIO_GOOGLE_MAPS_API_KEY || '',
     }),
   ],
-
   schema: {
     types: schemaTypes,
   },
